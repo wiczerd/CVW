f5bdfec6810b5a8f55011aee1d747586a79ee362
wiczerd <wiczerd@gmail.com>
2016-10-17 01:37:14 -0400

    cyclical differences in annual earnings changs

--- earnmChange.R
+++ earnmChange.R
@@ -19,8 +19,8 @@ setwd(rootdir)
 
 sipp <- readRDS(paste0(outputdir,"/sipp_2.RData"))
 
-# select relevant variables, exclude recessions
-sipp <- sipp[!(recIndic), c("panel","year","month","id","earnm","lfstat","wpfinwgt"), with=FALSE]
+# select relevant variables
+sipp <- sipp[, c("panel","year","month","id","earnm","lfstat","wpfinwgt", "recIndic","recIndic2"), with=FALSE]
 
 # only keep those there for a full year (take away all recession years entirely?)
 sipp[, monthsInYr := .N, by=list(id,year)]
@@ -29,7 +29,7 @@ sipp[, monthsInLF := sum(lfstat==1 & earnm>0), by=list(id,year)]
 sipp <- sipp[monthsInYr == 12 & monthsInLF>=3,]
 
 # collapse to yearly frequency (sum of earnm, mean of weights)
-sipp <- sipp[, list(earnm = sum(earnm, na.rm=TRUE),
+sipp <- sipp[, list(earnm = sum(earnm, na.rm=TRUE),recIndic2= all(recIndic2,na.rm=T),
 		    wpfinwgt = mean(wpfinwgt, na.rm=TRUE)), by=list(id,year)]
 
 # get earnings quantile by year, remove 0's first
@@ -53,7 +53,7 @@ ggplot(df[earnmQ>=.05 & earnmQ<=1.0,], aes(earnmQ, earnmChg,color=factor(qtl),gr
 	ggtitle("Percentiles of Annual Earnings Change by Base-Percentile") +
 	xlab("Earnm percentile in base year") +
 	ylab("Average percent change")+
-	scale_color_manual(values = c("steelblue","black", "violetred"),
+	scale_color_manual(values = c("mediumpurple","purple","mediumpurple"),
 					   labels = c("10","Median","90"),
 					   name = "")
 ggsave(paste0(figuredir, "/earnmPctile_trunc2.png"))
@@ -70,6 +70,24 @@ ggplot(df2[earnmQ>=.05 & earnmQ<=1.0,], aes(earnmQ, earnmStd)) +
 	xlab("Earnm percentile in base year") +
 	ylab("Std dev of average percent change")
 ggsave(paste0(figuredir, "/earnmStd_trunc2.png"))
+
+#split it by recession and expansion
+dfr <- sipp[, .(earnmChg = wtd.quantile(earnmChg, weights=wpfinwgt, probs=c(.1,.5,.9), na.rm=T)), by=list(earnmQ,recIndic2)]
+dfr$qtl <- rep(c(10L,50L,90L),202)
+
+# plot
+ggplot(dfr[earnmQ>=.05 & earnmQ<=1.0,], aes(earnmQ, earnmChg,color=interaction(qtl,recIndic2),group=interaction(qtl,recIndic2))) + 
+	geom_point() + 
+	geom_line() +
+	ylim(c(-1,2)) +
+	geom_hline(yintercept=0) +
+	ggtitle("Percentiles of Annual Earnings Change by Base-Percentile") +
+	xlab("Earnm percentile in base year") +
+	ylab("Average percent change")+
+	scale_color_manual(values = c("steelblue","blue", "steelblue","palevioletred","red","palevioletred"),
+					   labels = rep(c("10","Median","90"),2),
+					   name = "")
+ggsave(paste0(figuredir, "/earnmPctile_rec_trunc2.png"))
 
 
 # check that distribution is as expected
